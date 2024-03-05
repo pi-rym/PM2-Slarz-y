fccff362f6ffabdaef9781ab1057dda251e4b048
Slarz-y <luix0416@gmail.com>
2024-03-05 01:01:09 -0500

    Adelanto del primer Homework de M2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -6,25 +6,60 @@ let terceraPelicula = document.getElementById("terceraPelicula");
 
 let arrPeliculas = [primeraPelicula, segundaPelicula, terceraPelicula]
 
-tempData.map(function (pelicula, indice) {
+tempData.forEach((pelicula, indice) => {
     let elemento = arrPeliculas[indice];
-    
+
     let primerDiv = document.createElement("div");
-    primerDiv.className("box");
+    primerDiv.classList.add("box");
     let segundoDiv = document.createElement("div");
-    segundoDiv.className("box");
+    segundoDiv.classList.add("box");
     let tercerDiv = document.createElement("div");
-    tercerDiv.className("box");
+    tercerDiv.classList.add("box");
 
     let h2 = document.createElement("h2");
-    h2.className("titulo-pelicula");
+    h2.classList.add("titulo-pelicula");
+    h2.textContent = pelicula.title;
+
     let urlImg = document.createElement("img");
-    urlImg.className("imagen-pelicula")
+    urlImg.classList.add("imagen-pelicula");
+    urlImg.src = pelicula.poster;
+
     let descripcion = document.createElement("p");
-    let datos  = document.createElement("p");
+    descripcion.classList.add("descripcion-pelicula");
+    descripcion.textContent = pelicula.description;
+
+    let generos = document.createElement("p");
+    generos.classList.add("descripcion-pelicula");
+    generos.textContent = "Generos: " + pelicula.genre;
+
+    let año = document.createElement("p");
+    año.classList.add("datos-relevantes");
+    año.textContent = "Año: " + pelicula.year;
+
+    let director = document.createElement("p");
+    director.classList.add("datos-relevantes");
+    director.textContent = "Director: " + pelicula.director;
+
+    let duracion = document.createElement("p");
+    duracion.classList.add("datos-relevantes");
+    duracion.textContent = "Duracion: " + pelicula.duration;
+
+    let calificacion = document.createElement("p");
+    calificacion.classList.add("datos-relevantes");
+    calificacion.textContent = "Calificacion: " + pelicula.rate;
     
+    primerDiv.appendChild(h2);
+    primerDiv.appendChild(descripcion);
+    primerDiv.appendChild(generos)
+    segundoDiv.appendChild(urlImg);
+    tercerDiv.appendChild(director);
+    tercerDiv.appendChild(año);
+    tercerDiv.appendChild(duracion);
+    tercerDiv.appendChild(calificacion);
 
-
+    elemento.appendChild(primerDiv);
+    elemento.appendChild(segundoDiv);
+    elemento.appendChild(tercerDiv);
 })
 
 
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -4,6 +4,8 @@ const tempData = [
     year: 2017,
     director: "James Gunn",
     duration: "2h 16min",
+    description: 
+    "Continúan las aventuras del equipo en su travesía por los confines del cosmos. Los Guardianes deberán luchar para mantener unida a su nueva familia mientras intentan resolver el misterio de los verdaderos orígenes de Peter Quill. Viejos rivales se convertirán en nuevos aliados, y queridos personajes de los cómics clásicos acudirán en ayuda de nuestros héroes a medida que el universo cinematográfico de Marvel continúa expandiéndose.",
     genre: ["Action", "Adventure", "Comedy"],
     rate: 7.7,
     poster:
@@ -14,6 +16,8 @@ const tempData = [
     year: 1977,
     director: "George Lucas",
     duration: "2h 1min",
+    description: 
+    "La princesa Leia, líder del movimiento rebelde que desea reinstaurar la República en la galaxia, es capturada por las Fuerzas Imperiales de Darth Vader. El joven Luke Skywalker, ayudado por Han Solo y los androides, R2D2 y C3PO, serán los encargados de luchar e intentar rescatarla.",
     genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
     rate: 8.7,
     poster:
@@ -24,6 +28,8 @@ const tempData = [
     year: 2001,
     director: "Peter Jackson",
     duration: "2h 58min",
+    description: 
+    "En la Tierra Media, el Señor Oscuro Sauron ordenó a los Elfos que forjaran los Grandes Anillos de Poder. Tres para los reyes Elfos, siete para los Señores Enanos, y nueve para los Hombres Mortales. Pero Saurón también forjó, en secreto, el Anillo Único, que tiene el poder de esclavizar toda la Tierra Media.",
     genre: ["Action", "Adventure", "Drama", "Fantasy"],
     rate: 8.8,
     poster:
